4ab8ebc4bf405733e7fceee9e3ce9c844e7b28f6
Jeremy Allard <jeremy@allard.io>
2019-02-26 16:12:02 -0500

    Add new story in storybook.
    Implement new story that shows how <FormContainer /> and
    <FormComponent /> could look like when implemented as part of a bigger
    application.

--- stories/index.tsx
+++ stories/index.tsx
@@ -1,28 +1,62 @@
 import '@storybook/addon-actions'
-import centered from "@storybook/addon-centered"
+import centered from '@storybook/addon-centered'
 import { text, withKnobs } from '@storybook/addon-knobs/react'
 import { storiesOf } from '@storybook/react'
 import React from 'react'
-import { Helmet } from 'react-helmet'
+import { FaArrowLeft, FaInfoCircle } from 'react-icons/fa'
+import { Box, Button, Flex, Text } from 'rebass'
 import { FormComponent, FormContainer } from '../src/index'
 import Input from '../src/Input'
 
-storiesOf('Payment form', module)
-  .addDecorator(withKnobs)
-  .addDecorator(centered)
-  .add('with default credentials', () => {
+const DefaultContainer = _ => (
+  <FormContainer
+    clientKey={process.env.AUTHORIZENET_CLIENTKEY}
+    apiLoginId={process.env.AUTHORIZENET_LOGINID}
+    environment="sandbox"
+    component={FormComponent}
+    amount={25}
+  />
+)
+
+storiesOf('FormContainer', module)
+  .add('standalone', () => {
+    return <DefaultContainer />
+  })
+  .add('as a payment step', () => {
     return (
-      <div style={{width: "700px" }}>
-        <Helmet>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-        </Helmet>
-        <FormContainer
-          clientKey={text('clientKey', process.env.AUTHORIZENET_CLIENTKEY)}
-          apiLoginId={text('apiLoginId', process.env.AUTHORIZENET_LOGINID)}
-          environment="sandbox"
-          component={FormComponent}
-          amount={25}
-        />
+      <div
+        style={{ height: '100vh', backgroundColor: '#6772e5', padding: '20px' }}
+      >
+        <div
+          style={{
+            alignItems: 'center',
+            borderRadius: '4px',
+            cursor: 'pointer',
+            display: 'inline-flex',
+            margin: '1em 0em',
+            padding: '16px'
+          }}
+        >
+          <FaArrowLeft color="white" fontSize={'1.5em'} />
+          <Text color="white" fontFamily="roboto" pl={2}>
+            back
+          </Text>
+        </div>
+        <Flex alignItems="center" px={[3, 5] as any} py={[3, 0] as any}>
+          <FaInfoCircle color="white" fontSize="2em" />
+          <Text
+            color="white"
+            fontFamily="roboto"
+            fontSize={4}
+            fontWeight={'500'}
+            textAlign="center"
+            p={3}
+          >
+            Payment details
+          </Text>
+        </Flex>
+
+        <DefaultContainer />
       </div>
     )
   })
